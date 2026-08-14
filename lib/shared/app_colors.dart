10e02239a2a1732cd0d27646395066648d62cf71
vdmsantos <vitor6890@gmail.com>
2026-08-14 19:56:11 -0300

    feat: Add AppRequiredPassword widget and enhance text styles with grey and green options

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static final Color black = Colors.black;
   static final Color white = Colors.white;
   static final Color grey100 = Color(0xFFE0E0E0);
+  static final Color grey = Colors.grey;
+  static final Color green = Colors.green;
 }
