0749ac7216da3790bbfcb1024d35e82650062c04
vdmsantos <vitor6890@gmail.com>
2026-08-21 18:54:38 -0300

    feat: Refactor login handling to use ChangeNotifier and improve state management

--- lib/features/login/controllers/login_controller.dart
+++ lib/features/login/controllers/login_controller.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class LoginController {
+class LoginController extends ChangeNotifier {
   final RegExp _emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
   final int _caracterMinimoSenha = 6;
   TextEditingController emailController = TextEditingController();
   TextEditingController senhaController = TextEditingController();
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+
   bool isActiveCheckBox = false;
 
   bool isLoading = false;
@@ -15,6 +17,18 @@ class LoginController {
 
   void changeActiveCheckBox() {
     isActiveCheckBox = !isActiveCheckBox;
+    notifyListeners();
+  }
+
+  Future<void> handleLogin() async {
+    if (key.currentState!.validate()) {
+      isLoading = true;
+      notifyListeners();
+
+      await login();
+      isLoading = false;
+      notifyListeners();
+    }
   }
 
   Future<void> login() async {
